0347824e9cfae02c673404abdf9865e3694903bd
alor <alor@hackingteam.it>
2011-01-14 14:07:53 +0100

    tests for DB

---
+++ test/test_db.rb
@@ -0,0 +1,18 @@
+require 'helper'
+
+class TestDB < Test::Unit::TestCase
+
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+  def teardown
+    # Do nothing
+  end
+
+
+end
