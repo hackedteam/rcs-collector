6352fe25f8b0b0c288f797f15e4e069a7250dfe9
alor <alor@hackingteam.it>
2011-01-18 16:39:39 +0100

    unified loader for encrypted and plain code

--- lib/rcs-collector.rb
+++ lib/rcs-collector.rb
@@ -1,2 +1,13 @@
-require 'rcs-collector/collector.rb'
+
+# release file are encrypted and stored in a different directory
+if File.directory?(Dir.pwd + '/lib/rcs-collector-release')
+  require 'rcs-collector-release/collector.rb'
+# otherwise we are using development code
+elsif File.directory?(Dir.pwd + '/lib/rcs-collector')
+  puts "WARNING: Executing clear text code... (debug only)"
+  require 'rcs-collector/collector.rb'
+else
+  puts "FATAL: cannot find any rcs-collector code!"
+end
+
 
