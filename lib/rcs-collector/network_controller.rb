33220f5877c6a28c5f5d0180833234d1252bc810
alor <alor@hackingteam.it>
2011-02-03 13:45:43 +0100

    avoid the EM to stop on network check. implemented as thread

--- lib/rcs-collector/network_controller.rb
+++ lib/rcs-collector/network_controller.rb
@@ -6,6 +6,9 @@
 require 'rcs-common/trace'
 
 # system
+require 'socket'
+require 'openssl'
+require 'timeout'
 
 module RCS
 module Collector
@@ -14,40 +17,78 @@ class NetworkController
   extend RCS::Tracer
 
   def self.check
-    # send the status to the db
-    send_status
+    # we are called from EventMachine, create a thread and return as soon as possible
+    Thread.new do
 
-    # retrieve the lists from the db
-    elements = DB.instance.proxies
-    elements += DB.instance.anonymizers
 
-    threads = []
+      # retrieve the lists from the db
+      elements = DB.instance.proxies
+      elements += DB.instance.anonymizers
 
-    # keep only the remote anonymizers discarding the local collectors
-    elements.delete_if {|x| x['type'] == 'LOCAL'}
+      threads = []
 
-    # keep only the elements to be polled
-    #elements.delete_if {|x| x['poll'] == 0}
+      # keep only the remote anonymizers discarding the local collectors
+      elements.delete_if {|x| x['type'] == 'LOCAL'}
 
-    if not elements.empty? then
-      trace :info, "[NC] Checking #{elements.length} network elements..."
+      # keep only the elements to be polled
+      #elements.delete_if {|x| x['poll'] == 0}
+
+      if not elements.empty? then
+        trace :info, "[NC] Checking #{elements.length} network elements..."
+        # send the status to the db
+        send_status "Checking #{elements.length} network elements..."
+      else
+        # send the status to the db
+        send_status "Idle..."
+      end
+
+      # contact every element
+      elements.each do |p|
+        threads << Thread.new {
+          begin
+            Timeout::timeout(Config.instance.global['NC_INTERVAL'].to_i / 2) do
+              check_element p
+            end
+          rescue Exception => e
+            trace :warn, "[NC] #{p['address']} #{e.message}"
+          end
+          # make sure to destroy the thread after the check
+          Thread.exit
+        }
+      end
+
+      # wait for all the threads to finish
+      threads.each do |t|
+        t.join
+      end
+
+      trace :info, "[NC] Network elements check completed"
+
+      # ensure to exit
+      Thread.exit
     end
-
-    # contact every element
-    elements.each do |p|
-      threads << Thread.new {
-        #TODO: implement the real check
-        puts p.inspect
-      }
-    end
-
-    # wait for all the threads to finish
-    threads.each do |t|
-      t.join
-    end
-
-    trace :info, "[NC] Network elements check completed"
   end
+
+
+  def self.check_element(element)
+    #TODO: implement the real check
+    puts element.inspect
+
+    socket = TCPSocket.new(element['address'], element['port'])
+
+    ssl_context = OpenSSL::SSL::SSLContext.new()
+    ssl_context.cert = OpenSSL::X509::Certificate.new(File.read(Dir.pwd + "/config/" + Config.instance.global['DB_CERT']))
+    #ssl_context.key = OpenSSL::PKey::RSA.new(File.open("keys/MyCompanyClient.key"))
+    ssl_socket = OpenSSL::SSL::SSLSocket.new(socket, ssl_context)
+    ssl_socket.sync_close = true
+    ssl_socket.connect
+
+    ssl_socket.puts("GET / HTTP/1.0")
+    #ssl_socket.puts("")
+
+    ssl_socket.close
+  end
+
 
   def self.push(host, content)
     #TODO: implement the real push
@@ -56,13 +97,10 @@ class NetworkController
     return "PUSHED", "text/html"
   end
 
-  def self.send_status
+  def self.send_status(message)
     # report our status to the db
     component = "RCS::NetworkController"
     ip = ''
-
-    # always idle
-    message = "Idle..."
 
     # report our status
     status = Status.my_status
