ad87547f37e64689c72441fe0770c75ab1d54e27
Fabrizio Cornelli <finance@suppose.it>
2012-06-20 09:57:32 +0200

    Double core is better than one

--- lib/rcs-collector/db.rb
+++ lib/rcs-collector/db.rb
@@ -346,9 +346,9 @@ class DB
     return (upgrades.nil? or upgrades.empty?) ? false : true
   end
 
-  def new_upgrade(bid)
+  def new_upgrade(bid, flavor)
     # retrieve the uploads from the cache
-    upgrade, left = DBCache.new_upgrade bid
+    upgrade, left = DBCache.new_upgrade(bid, flavor)
 
     return nil if upgrade.nil?
 
@@ -357,6 +357,7 @@ class DB
 
     # delete from the db only if all the file have been transmitted
     if left == 0 then
+      DBCache.del_upgrade bid
       db_rest_call :del_upgrade, bid if @available
     end
 
