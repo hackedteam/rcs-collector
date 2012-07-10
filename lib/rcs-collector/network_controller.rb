94e0c744dff7182f09bf462a6e20496f615f30bc
alor <alberto.ornaghi@gmail.com>
2012-07-10 10:37:07 +0200

    trivial

--- lib/rcs-collector/network_controller.rb
+++ lib/rcs-collector/network_controller.rb
@@ -91,6 +91,7 @@ class NetworkController
 
   def self.check_element(element)
 
+    # TODO: remove in 8.3
     # be sure to have the network certificate
     DB.instance.get_network_cert(Config.instance.file('rcs-network')) unless File.exist? Config.instance.file('rcs-network.pem')
 
