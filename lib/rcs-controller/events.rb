66208167731047f42704fced7723908c4df5e1fb
topac <topac@hackingteam.com>
2013-11-29 11:56:06 +0100

    avoid defer each time the periodictimer tick

--- lib/rcs-controller/events.rb
+++ lib/rcs-controller/events.rb
@@ -1,48 +1,28 @@
-#
-#  Event handlers
-#
-
-# from RCS::Common
 require 'rcs-common/trace'
 require 'rcs-common/systemstatus'
 
 require_relative 'network'
 
-# system
 require 'eventmachine'
 
 module RCS
-module Controller
+  module Controller
+    class Events
+      include RCS::Tracer
 
-class Events
-  include RCS::Tracer
-  
-  def setup(port = 80)
+      def check_interval
+        Config.instance.global['NC_INTERVAL']
+      end
 
-    # main EventMachine loop
-    begin
-      # all the events are handled here
-      EM::run do
-        # if we have epoll(), prefer it over select()
+      def setup
         EM.epoll
 
-        # we are alive and ready to party
-        SystemStatus.my_status = SystemStatus::OK
+        EM::run do
+          SystemStatus.my_status = SystemStatus::OK
 
-        # first heartbeat and checks
-        EM.defer(proc{ Network.check })
-        # subsequent checks
-        EM::PeriodicTimer.new(Config.instance.global['NC_INTERVAL']) { EM.defer(proc{ Network.check }) }
-
+          EM::PeriodicTimer.new(check_interval) { Network.check }
+        end
       end
-    rescue Exception => e
-      raise
     end
-
   end
-
-end #Events
-
-end #Collector::
-end #RCS::
-
+end
