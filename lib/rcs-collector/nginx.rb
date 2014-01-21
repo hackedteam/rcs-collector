b2be5208658f3468d15dba2b2a1690a50dcdc584
alor <alberto.ornaghi@gmail.com>
2014-01-21 15:08:58 +0100

    todo

---
+++ lib/rcs-collector/nginx.rb
@@ -0,0 +1,30 @@
+
+
+module RCS
+  module Collector
+    module Nginx
+
+      def start
+
+      end
+
+      def stop
+
+      end
+
+      def reload
+
+      end
+
+      def save_conf
+
+      end
+
+      def config
+
+      end
+
+
+    end
+  end
+end
