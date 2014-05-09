f205742b4044c1ffc8ec2998d6a82448a8815d48
topac <topac@hackingteam.com>
2014-05-09 10:23:49 +0200

    sync stat

---
+++ lib/rcs-collector/sync_stat.rb
@@ -0,0 +1,52 @@
+module RCS
+  module Collector
+    class SyncStat
+
+      attr_accessor :total
+
+      def initialize
+        @total = 0
+        @transferred = 0
+        @transferred_size = 0
+      end
+
+      def ended
+        @ended_at = timestamp
+      end
+
+      def timedout
+        @timeout = true
+        ended
+      end
+
+      def started
+        @started_at = timestamp
+      end
+
+      def update(evidence_size, num: 1)
+        @transferred += num
+        @transferred_size += evidence_size
+
+        transfer_time = timestamp - @started_at
+
+        # bytes/sec
+        @speed = @transferred_size / transfer_time
+      end
+
+      def to_hash
+        {total: @total, count: @transferred, started_at: @started_at,
+          ended_at: @ended_at, speed: @speed, timeout: @timeout, size: @transferred_size}
+      end
+
+      alias :as_json :to_hash
+
+      def to_json(*args)
+        as_json.to_json(args)
+      end
+
+      def timestamp
+        Time.now.utc.to_f
+      end
+    end
+  end
+end
